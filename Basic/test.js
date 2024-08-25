dff636cb159fe8f5d8d4cd1240a42cb13d6d7967
Chaitali Jadhav <chaitalijadhav0254@gmail.com>
2024-08-25 10:29:56 +0000

    Variables

---
+++ Basic/test.js
@@ -0,0 +1,7 @@
+const accountId=12345
+let accountEmail="acc123@gmail.com"
+var accountPass="98765"
+accountCity="Latur"
+accountStaten
+console.log(accountId)
+console.table([accountId, accountEmail, accountPass, accountCity, accountState])
